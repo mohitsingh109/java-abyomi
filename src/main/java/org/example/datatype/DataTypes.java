0cc0d8301cc1d371e106e5760f1818970e6f8b88
Homely Assist <homeconnecteasy@gmail.com>
2024-07-29 07:05:12 +0530

    pushing data types

---
+++ src/main/java/org/example/datatype/DataTypes.java
@@ -0,0 +1,31 @@
+package org.example.datatype;
+
+// OOPS
+public class DataTypes {
+
+    public static void main(String[] args) { // First function that run in java (JVM)
+
+        // short
+        short value = 9999; // age
+
+        System.out.println(value);
+        // int
+        int age = 999999999; // (-23858)
+        System.out.println(age);
+        // long
+        long amount = 10000000000L; //youtube views
+        // float
+        float value1 = 34.5f;
+        System.out.println(value1);
+        //double
+        double value2 = 999999999.568;
+        System.out.printf("%.1f",value2);
+        // char
+        char a = 'a';
+        char b = 'b';
+
+        // boolean
+        boolean value3 = false; // true
+        value3 = true;
+    }
+}
